ede52382f518fabde9ccdd96d19d71c1e1c7a5bd
Daniel Mack <daniel.mack@powerhrg.com>
2018-08-23 18:35:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'f40b3c95f59b8e715c2bb53d2efc6b3f17ee9ba4'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));

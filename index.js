97ee48cd575c1ab0d9d7884ea6914515a66a4a5f
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-08-26 17:59:44 -0400

    finished ajax fetch readme

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much.";
+const token = 'cf5d8aa7c09360bf9439c57ec0c5f953d0f3b943'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));

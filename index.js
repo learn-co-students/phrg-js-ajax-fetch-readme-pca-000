26135ccea7c80db6d2aaebc2577674d45e16df09
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-08-30 12:22:42 -0400

    yea this makes way more sense

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '63418df250b50a6009dca17a73669393f0587dd3'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
